bbc400b31d356a88f472080e7fc0e42a032bb852
Alicia Barczak <aliciabarczak@Alicias-MacBook-Air.local>
2022-06-13 15:23:51 +0100

    add essencial CSS to ArticleCard for clarity

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,11 +1,13 @@
 export default function ArticleCard({title, author, created_at, comment_count, votes}) {
     return (
-        <>
-        <h2>{title}</h2>
-        <h3>{author}</h3>
-        <h4>Posted on {created_at}</h4>
-        <h5>Likes({votes})</h5>
-        <h5>comments({comment_count})</h5>
-        </>
+        < div className="ArticleCard">
+            <section className="cardText">
+                <h2>{title}</h2>
+                <h3>by {author}</h3>
+                <h4>Posted on {created_at.split("T")[0]}</h4>
+                <h5>Likes({votes})</h5>
+                <h5>Comments({comment_count})</h5>
+        </section>
+        </div>
     )
 }
