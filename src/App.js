3a925be86a4c2aa25a4c38c8ccaa5398877bf6f6
Alicia Barczak <aliciabarczak@Alicias-MacBook-Air.local>
2022-06-13 16:19:55 +0100

    added route path for the homepage

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import './App.css';
 import Header from "./components/Header.jsx"
 import Nav from "./components/Nav.jsx"
 import ListOfArticles from "./components/ListOfArticles.jsx"
+import {Routes, Route} from "react-router-dom";
 
 function App() {
+  
   return (
     <div className="App">
       <Header />
       <Nav />
-      <ListOfArticles />
+      <Routes>
+        <Route path="/" element={ <ListOfArticles/>} />
+      </Routes>
     </div>
-  );
-}
+  )
+};
 
 export default App;
