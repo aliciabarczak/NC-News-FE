575d619bd4a1514cbdc5ad9f9e296c99fac1d396
Alicia Barczak <aliciabarczak@Alicias-MacBook-Air.local>
2022-06-14 14:33:32 +0100

    add loading pattern

--- src/components/CommentsCard.jsx
+++ src/components/CommentsCard.jsx
@@ -1,18 +1,21 @@
 import {useState, useEffect} from "react";
-import {getComments} from "./../Utils/api.js"
+import {getComments, getArticleById} from "./../Utils/api.js"
 import { useParams } from "react-router-dom";
 
 export default function CommentsCard(){
 const [allComments, setAllComments] = useState([]);
 const { article_id } = useParams(); 
+const [isLoading, setIsLoading] = useState(true);
 
 
 useEffect(() => {
     getComments(article_id).then(({comments}) => {
         setAllComments(comments)
+        setIsLoading(false);  
     })
 }, [article_id])
 
+if(isLoading) return <p>...loading</p>
 return (
     <>
     <ul className="Comments">
