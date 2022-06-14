e1b081c471617a3ca69ff28840515fb1b882fd7a
Alicia Barczak <aliciabarczak@Alicias-MacBook-Air.local>
2022-06-14 10:26:23 +0100

    add filter by topic functionality

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,29 +1,24 @@
 import {Link} from "react-router-dom";
-import {useState} from "react";
+import {useState, useEffect} from "react";
+import { getTopics } from "./../Utils/api.js"
 
 export default function Nav() {
-
-    const [topics, setTopics] = useState([
-        {
-        "slug": "coding",
-        "description": "Code is love, code is life"
-        },
-        {
-        "slug": "football",
-        "description": "FOOTIE!"
-        }
-    ])
+const [topics, setTopics] = useState([])
+    useEffect(() => {
+        getTopics().then(({topics}) => {
+            setTopics(topics)
+        })
+    }, [])
 
     return (
         <nav className="Nav">
             <Link to="/">Home</Link>
-            {topics.map(({slug}) => {
+            {topics.map(({slug}, index) => {
                 return (
-                    <Link to="/">{slug}</Link>
+                    <Link to={`articles?topic=${slug}`} key={index}>{slug}</Link>
                 )
             })
         }
-
         </nav>
     )
 }
