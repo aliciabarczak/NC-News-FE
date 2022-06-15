bc395051c4c8ccd2bc90cb6d0c445977270fb04f
Alicia Barczak <aliciabarczak@Alicias-MacBook-Air.local>
2022-06-15 11:12:55 +0100

    further layout changes to accomondate sortBy

--- src/components/SortBy.jsx
+++ src/components/SortBy.jsx
@@ -1,18 +1,22 @@
 export default function SortBy() {
     return (
-    <form >
-    <label> Sort by:
-      <select>
+    <form className="SortByForm">
+    <label className="dropDown"> Sort by:
+      <select className="dropDownMenu">
         <option>Date</option>
         <option>Comment Count</option>
         <option>Votes</option>
       </select>
     </label>
-    <label for="ascending">
+    <label className="radio">
+    <div className="Ascending">
     Ascending
-    <input type="radio" value="ascending" name="ascending"></input>
+    <input className="radio-btn" type="radio" value="ascending" name="ascending/descending"></input>
+    </div>
+    <div className="Descending">
     Descending
-    <input type="radio" value="discending" name="discending"></input>
+    <input className="radio-btn" type="radio" value="descending" name="ascending/descending"></input>
+    </div>
     </label>
   </form> 
     
